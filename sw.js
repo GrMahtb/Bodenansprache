bbd9ead0bc210eec49458c8dab9225246f053e36
GrMahtb <gregorb.mair@gmail.com>
2026-07-01 08:35:34 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const CACHE_NAME = 'bohrkern-v158';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json',
+  './logo.svg',
+  './icon.svg',
+  './launchericon-192x192.png',
+  './launchericon-512x512.png',
+  './LiberationSans-Regular.ttf',
+  './LiberationSans-Bold.ttf'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : null))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  const url = new URL(event.request.url);
+
+  if (url.origin !== self.location.origin) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(event.request)
+        .then((response) => {
+          if (response && response.ok) {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(() => {});
+          }
+          return response;
+        })
+        .catch(() => caches.match('./index.html'));
+    })
+  );
+});
